ec4570ca4b6a3ddbdd8b0532ddbb3a6d1ee89a3f
Gerit Wagner <gerit.wagner@stud.uni-regensburg.de>
2014-04-20 20:36:59 +0200

    Design About

--- _code/main/About.java
+++ _code/main/About.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 /**
  * @author Honors-WInfo-Projekt (Fabian Böhm, Alexander Puchta), Gerit Wagner
@@ -13,14 +14,29 @@ public class About extends JDialog {
 	private static final long serialVersionUID = 1L;
 	private Dimension d = getToolkit().getScreenSize();
 	
+	
 	public About() {
 	    super(Hauptfenster.aktuellesHauptfenster, "About", true);
 	
 	    Box b = Box.createVerticalBox();
 	    b.add(Box.createGlue());
-	    b.add(new JLabel("SWert Version " + Hauptfenster.Version));
-	    b.add(new JLabel("Contributors:" + Hauptfenster.Autoren));
-	    b.add(new JLabel("License: t.b.d."));
+	    
+	    
+	    EmptyBorder eBorder = new EmptyBorder(2, 10, 2, 10);
+        JLabel version = new JLabel("SWert Version " + Hauptfenster.Version);
+        version.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(), eBorder)); 
+
+        JLabel authors = new JLabel("Entwickler: " + Hauptfenster.Autoren);
+        authors.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(), eBorder)); 
+        
+        JLabel license = new JLabel("Lizenz: GPL?");
+        // add license-link?
+        license.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(), eBorder)); 
+
+        b.add(version);
+	    b.add(authors);
+	    b.add(license);
+	    
 	    b.add(Box.createGlue());
 	    getContentPane().add(b, "Center");
 	
@@ -32,6 +48,7 @@ public class About extends JDialog {
 	    ok.addActionListener(new ActionListener() {
 	      public void actionPerformed(ActionEvent evt) {
 	        setVisible(false);
+	        dispose();
 	      }
 	    });
 	    setResizable(false);
